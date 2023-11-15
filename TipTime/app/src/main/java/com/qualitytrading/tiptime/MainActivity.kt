8be0f42fcfff5c13ebc9131f8ec3911df89be036
adriaanbakker-java <adriaan.bakker@hotmail.com>
2023-11-15 12:15:10 +0100

    androidprojects

---
+++ TipTime/app/src/main/java/com/qualitytrading/tiptime/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.qualitytrading.tiptime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.qualitytrading.tiptime.ui.theme.TipTimeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TipTimeLayout()
+                }
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun EditNumberField(modifier: Modifier = Modifier) {
+        var amountInput by remember {mutableStateOf("0") }
+        TextField(
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.bill_amount)) },
+            value = amountInput,
+            onValueChange = {
+                            amountInput = it
+            },
+            modifier = modifier
+        )
+    }
+
+
+
+    @Composable
+    private fun TipTimeLayout() {
+        Column() {
+            Text(
+                text = stringResource(R.string.calculate_tip),
+                modifier = Modifier
+                    .padding(bottom = 16.dp, top = 40.dp)
+                    .align(alignment = Alignment.Start)
+            )
+            EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+            Text(
+                text = stringResource(R.string.tip_amount, "$0x.00"),
+                style = MaterialTheme.typography.displaySmall
+            )
+            Spacer(modifier = Modifier.height(150.dp))
+
+        }
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        TipTimeTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                TipTimeLayout()
+            }
+        }
+    }
+}
